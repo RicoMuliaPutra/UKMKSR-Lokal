6387ccfc38ac7769c3b0b62f9841007c8fc5c1ea
baguslaksana <arylaksana27@gmail.com>
2025-04-25 01:33:37 +0700

    Galeri Done

--- resources/views/admin/tentang/index.blade.php
+++ resources/views/admin/tentang/index.blade.php
@@ -21,53 +21,64 @@
     @extends('admin.layout.navbar')
 
     @section('content')
-    <div class="container mx-auto">
-        <h1 class="mb-4 text-2xl font-bold">Tentang</h1>
+    <div class="max-w-6xl mx-auto py-5 px-4">
 
-        <div class="p-4 bg-white rounded-lg shadow-md">
-            <div class="flex items-center justify-between mb-4">
+        <!-- Judul Halaman -->
+        <div class="text-center mb-8">
+            <div class="bg-gradient-to-r from-red-600 to-red-300 text-white text-xl font-bold py-4 rounded-2xl shadow-lg">
+                Tentang UKM KSR
+            </div>
+        </div>
+
+        <!-- Box Tentang UKM KSR-->
+        <div class="bg-white shadow-xl rounded-3xl p-6 mt-10">
+            <div class="flex justify-between items-center mb-6">
                 <h2 class="text-lg font-semibold text-gray-700">Tentang UKM KSR</h2>
-                <a href="{{ route('tentang.create') }}"
-                    class="px-3 py-1 text-sm text-white transition bg-blue-500 rounded hover:bg-blue-600">
-                    + Tambah
-                </a>
             </div>
 
             <div class="overflow-x-auto">
-                <table class="w-full text-sm border border-collapse border-gray-300">
-                    <thead class="bg-gray-200">
+                <table class="min-w-full text-sm text-left">
+                    <thead class="bg-gray-200 text-gray-700 font-semibold">
                         <tr>
-                            <th class="p-2 border border-gray-300">No.</th>
-                            <th class="p-2 border border-gray-300">Deskripsi</th>
-                            <th class="p-2 border border-gray-300">Tanggal Publikasi</th>
-                            <th class="p-2 border border-gray-300">Aksi</th>
+                            <th class="px-4 py-3">No.</th>
+                            <th class="px-4 py-3">Deskripsi</th>
+                            <th class="px-4 py-3">Tanggal Publikasi</th>
+                            <th class="px-4 py-3 text-center">Aksi</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        @forelse ($data as $i => $item)
-                        <tr class="text-center bg-gray-50 hover:bg-gray-100">
-                            <td class="p-2 border border-gray-300">{{ $i + 1 }}</td>
-                            <td class="p-2 text-left border border-gray-300">{{ Str::limit($item->deskripsi_ksr, 100) }}</td>
-                            <td class="p-2 border border-gray-300">{{ \Carbon\Carbon::parse($item->created_at)->format('d M Y') }}</td>
-                            <td class="flex justify-center gap-2 p-2 border border-gray-300">
-                                <a href="{{ route('tentang.show', $item->id_tentang_ksr) }}"
-                                    class="text-blue-500 hover:text-blue-700"><i class="fas fa-info-circle"></i></a>
-
-                                <a href="{{ route('tentang.edit', $item->id_tentang_ksr) }}"
-                                    class="text-yellow-500 hover:text-yellow-700"><i class="fas fa-edit"></i></a>
-
-                                <form action="{{ route('tentang.destroy', $item->id_tentang_ksr) }}" method="POST"
-                                    onsubmit="return confirm('Yakin ingin menghapus data ini?')">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="text-red-500 hover:text-red-700"><i class="fas fa-trash"></i></button>
-                                </form>
+                    <tbody class="bg-white divide-y divide-gray-200">
+                        @forelse ($data as $key => $item)
+                        <tr class="hover:bg-gray-100 transition">
+                            <td class="px-6 py-3">{{ $key+1 }}</td>
+                            <td class="px-6 py-3">{{ $item->deskripsi_ksr }}</td>
+                            <td class="px-6 py-3">
+                                {{ \Carbon\Carbon::parse($item->created_at)->format('d M Y') }}
+                            </td>
+                            <td class="px-6 py-3 flex items-center space-x-3">
+                                <div class="flex justify-center space-x-3">
+                                    <a href="{{ route('tentang.show', $item->id_tentang_ksr) }}"
+                                        class="text-blue-600 hover:text-blue-800">
+                                        <i class="fas fa-info-circle"></i>
+                                    </a>
+                                    <a href="{{ route('tentang.edit', $item->id_tentang_ksr) }}"
+                                        class="text-yellow-500 hover:text-yellow-700">
+                                        <i class="fas fa-edit"></i>
+                                    </a>
+                                    <form action="{{ route('tentang.destroy', $item->id_tentang_ksr) }}" method="POST"
+                                        onsubmit="return confirm('Yakin ingin menghapus data ini?')">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="text-red-500 hover:text-red-700">
+                                            <i class="fas fa-trash"></i>
+                                        </button>
+                                    </form>
+                                </div>
                             </td>
                         </tr>
                         @empty
                         <tr>
-                            <td colspan="4" class="p-4 italic text-center text-gray-500 border border-gray-300">
-                                Belum ada data tentang KSR.
+                            <td colspan="4" class="px-6 py-6 text-center text-gray-500 italic">
+                                Belum ada data Tentang KSR.
                             </td>
                         </tr>
                         @endforelse
@@ -75,11 +86,182 @@
                 </table>
             </div>
         </div>
+
+        <!-- Box Info KSR -->
+        <div class="bg-white shadow-xl rounded-3xl p-6 mt-10">
+            <div class="flex justify-between items-center mb-6">
+                <h2 class="text-lg font-semibold text-gray-700">Info KSR</h2>
+            </div>
+
+            <div class="overflow-x-auto">
+                <table class="min-w-full text-sm text-left">
+                    <thead class="bg-gray-200 text-gray-700 font-semibold">
+                        <tr>
+                            <th class="px-4 py-3">No.</th>
+                            <th class="px-4 py-3">Deskripsi</th>
+                            <th class="px-4 py-3">Tanggal Publikasi</th>
+                            <th class="px-4 py-3 text-center">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200">
+                        @forelse ($data as $key => $item)
+                        <tr class="hover:bg-gray-100 transition">
+                            <td class="px-6 py-3">{{ $key+1 }}</td>
+                            <td class="px-6 py-3">{{ $item->deskripsi_ksr }}</td>
+                            <td class="px-6 py-3">
+                                {{ \Carbon\Carbon::parse($item->created_at)->format('d M Y') }}
+                            </td>
+                            <td class="px-6 py-3 flex items-center space-x-3">
+                                <div class="flex justify-center space-x-3">
+                                    <a href="{{ route('tentang.show', $item->id_tentang_ksr) }}"
+                                        class="text-blue-600 hover:text-blue-800">
+                                        <i class="fas fa-info-circle"></i>
+                                    </a>
+                                    <a href="{{ route('tentang.edit', $item->id_tentang_ksr) }}"
+                                        class="text-yellow-500 hover:text-yellow-700">
+                                        <i class="fas fa-edit"></i>
+                                    </a>
+                                    <form action="{{ route('tentang.destroy', $item->id_tentang_ksr) }}" method="POST"
+                                        onsubmit="return confirm('Yakin ingin menghapus data ini?')">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="text-red-500 hover:text-red-700">
+                                            <i class="fas fa-trash"></i>
+                                        </button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                        @empty
+                        <tr>
+                            <td colspan="4" class="px-6 py-6 text-center text-gray-500 italic">
+                                Belum ada data Info KSR.
+                            </td>
+                        </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <!-- Box Sejarah KSR -->
+        <div class="bg-white shadow-xl rounded-3xl p-6 mt-10">
+            <div class="flex justify-between items-center mb-6">
+                <h2 class="text-lg font-semibold text-gray-700">Sejarah KSR</h2>
+            </div>
+
+            <div class="overflow-x-auto">
+                <table class="min-w-full text-sm text-left">
+                    <thead class="bg-gray-200 text-gray-700 font-semibold">
+                        <tr>
+                            <th class="px-4 py-3">No.</th>
+                            <th class="px-4 py-3">Deskripsi</th>
+                            <th class="px-4 py-3">Tanggal Publikasi</th>
+                            <th class="px-4 py-3 text-center">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200">
+                        @forelse ($data as $key => $item)
+                        <tr class="hover:bg-gray-100 transition">
+                            <td class="px-6 py-3">{{ $key+1 }}</td>
+                            <td class="px-6 py-3">{{ $item->deskripsi_ksr }}</td>
+                            <td class="px-6 py-3">
+                                {{ \Carbon\Carbon::parse($item->created_at)->format('d M Y') }}
+                            </td>
+                            <td class="px-6 py-3 flex items-center space-x-3">
+                                <div class="flex justify-center space-x-3">
+                                    <a href="{{ route('tentang.show', $item->id_tentang_ksr) }}"
+                                        class="text-blue-600 hover:text-blue-800">
+                                        <i class="fas fa-info-circle"></i>
+                                    </a>
+                                    <a href="{{ route('tentang.edit', $item->id_tentang_ksr) }}"
+                                        class="text-yellow-500 hover:text-yellow-700">
+                                        <i class="fas fa-edit"></i>
+                                    </a>
+                                    <form action="{{ route('tentang.destroy', $item->id_tentang_ksr) }}" method="POST"
+                                        onsubmit="return confirm('Yakin ingin menghapus data ini?')">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="text-red-500 hover:text-red-700">
+                                            <i class="fas fa-trash"></i>
+                                        </button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                        @empty
+                        <tr>
+                            <td colspan="4" class="px-6 py-6 text-center text-gray-500 italic">
+                                Belum ada Sejarah KSR.
+                            </td>
+                        </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <!-- Box Visi - Misi KSR -->
+        <div class="bg-white shadow-xl rounded-3xl p-6 mt-10">
+            <div class="flex justify-between items-center mb-6">
+                <h2 class="text-lg font-semibold text-gray-700">Visi Misi KSR</h2>
+            </div>
+
+            <div class="overflow-x-auto">
+                <table class="min-w-full text-sm text-left">
+                    <thead class="bg-gray-200 text-gray-700 font-semibold">
+                        <tr>
+                            <th class="px-4 py-3">No.</th>
+                            <th class="px-4 py-3">Deskripsi</th>
+                            <th class="px-4 py-3">Tanggal Publikasi</th>
+                            <th class="px-4 py-3 text-center">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200">
+                        @forelse ($data as $key => $item)
+                        <tr class="hover:bg-gray-100 transition">
+                            <td class="px-6 py-3">{{ $key+1 }}</td>
+                            <td class="px-6 py-3">{{ $item->deskripsi_ksr }}</td>
+                            <td class="px-6 py-3">
+                                {{ \Carbon\Carbon::parse($item->created_at)->format('d M Y') }}
+                            </td>
+                            <td class="px-6 py-3 flex items-center space-x-3">
+                                <div class="flex justify-center space-x-3">
+                                    <a href="{{ route('tentang.show', $item->id_tentang_ksr) }}"
+                                        class="text-blue-600 hover:text-blue-800">
+                                        <i class="fas fa-info-circle"></i>
+                                    </a>
+                                    <a href="{{ route('tentang.edit', $item->id_tentang_ksr) }}"
+                                        class="text-yellow-500 hover:text-yellow-700">
+                                        <i class="fas fa-edit"></i>
+                                    </a>
+                                    <form action="{{ route('tentang.destroy', $item->id_tentang_ksr) }}" method="POST"
+                                        onsubmit="return confirm('Yakin ingin menghapus data ini?')">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="text-red-500 hover:text-red-700">
+                                            <i class="fas fa-trash"></i>
+                                        </button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                        @empty
+                        <tr>
+                            <td colspan="4" class="px-6 py-6 text-center text-gray-500 italic">
+                                Belum ada visi-misi KSR.
+                            </td>
+                        </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
     </div>
     @endsection
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
-
         function showImage(imageUrl) {
             Swal.fire({
                 imageUrl: imageUrl,
